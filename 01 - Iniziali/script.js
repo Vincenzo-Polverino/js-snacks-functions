890d6a2fd72ac27fa81b9e12cd2fe2fc680668ba
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-07 16:19:14 +0200

    create  for cicle in a function

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,10 +1,14 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+let initials
 
 
 // Dichiara la funzione qui.
+function find_initials(names){
+    for(let i= 0; i> names.length; i++){}
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
