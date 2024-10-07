d19dec4dde39e7308aea5a685506c2b01da42dde
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-07 16:27:45 +0200

    fix: cicle and colplitre function

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-let initials
 
+let initials= []
 
 // Dichiara la funzione qui.
 function find_initials(names){
-    for(let i= 0; i> names.length; i++){}
 
+    for(let i= 0; i< names.length; i++){
+        initials.push(names[i][0])
+    }
+    return initials
 }
 
 // Invoca la funzione qui e stampa il risultato in console
